6eda6b91387e302b1d573ee54473ccfe75988415
Luke Kreczko <lkreczko@googlemail.com>
2024-02-21 13:55:21 +0100

    refactor: use real config file for tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,26 +8,8 @@ from pydantic import ValidationError
 from fasthep_flow.config import FlowConfig, StageConfig, load_config
 
 
-# This would be a fixture providing various YAML configurations
-@pytest.fixture()
-def valid_config_yaml():
-    return """
-stages:
-  - name: printEcho
-    type: "fasthep_flow.operators.BashOperator"
-    kwargs:
-      bash_command: echo
-      arguments: ["Hello World!"]
-"""
-
-
-def test_load_config_parses_correctly(valid_config_yaml: str, tmp_path: Path):
-    # Create a temporary YAML config file
-    temp_config_file = tmp_path / "config.yaml"
-    temp_config_file.write_text(valid_config_yaml)
-
-    # Now call load_config with the path to the temporary file
-    parsed_config = load_config(temp_config_file)
+def test_load_config_parses_correctly(simple_config_yaml: Path):
+    parsed_config = load_config(simple_config_yaml)
     assert parsed_config is not None
     assert isinstance(parsed_config, FlowConfig)
 
@@ -39,13 +21,9 @@ def test_invalid_type():
     assert "Could not import invalid.type" in str(excinfo.value)
 
 
-def test_first_stage(valid_config_yaml: str, tmp_path: Path):
-    # Create a temporary YAML config file
-    temp_config_file = tmp_path / "config.yaml"
-    temp_config_file.write_text(valid_config_yaml)
-
+def test_first_stage(simple_config_yaml: Path):
     # Now call load_config with the path to the temporary file
-    parsed_config = load_config(temp_config_file)
+    parsed_config = load_config(simple_config_yaml)
     first_stage = parsed_config.stages[0]
     assert first_stage.name == "printEcho"
     assert first_stage.type == "fasthep_flow.operators.BashOperator"
