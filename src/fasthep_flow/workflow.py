f2d5f9b74b8c9985e4bf25446b24fbabd3be644d
Luke Kreczko <lkreczko@googlemail.com>
2024-02-22 14:43:19 +0100

    feat: first working draft of workflowgi

--- src/fasthep_flow/workflow.py
+++ src/fasthep_flow/workflow.py
@@ -1,14 +1,36 @@
 from __future__ import annotations
 
+from prefect import flow, task
+
 from .config import FlowConfig
 
 
 class Workflow:
+    tasks: list[task]
+
     def __init__(self, config: FlowConfig) -> None:
-        pass
+        stages = config.stages
+        self.tasks = []
+        for stage in stages:
+            self.tasks.append(
+                task(
+                    stage.resolve(),
+                    name=stage.name,
+                )
+            )
+
+    def __call__(self) -> None:
+        for t in self.tasks:
+            t()
 
     def run(self) -> None:
-        pass
+        f = flow(
+            self.__call__,
+            name="config name",
+            flow_run_name="fasthep-flow",
+            version="0.0.1",
+        )
+        f()
 
 
 def config_to_flow() -> None:
