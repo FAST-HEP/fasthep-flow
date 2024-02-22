af23cfe01ccc344df3405de90aed831064c99387
Luke Kreczko <lkreczko@googlemail.com>
2024-02-22 15:37:05 +0100

    fix: pydantic issues with prefect

--- src/fasthep_flow/workflow.py
+++ src/fasthep_flow/workflow.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from prefect import flow, task
+from typing import Any
 
 from .config import FlowConfig
 
@@ -8,14 +8,18 @@ from .config import FlowConfig
 class Workflow:
     """Wrapper for any compute graph implementation we want to support. Currently using Prefect."""
 
-    tasks: list[task]
+    tasks: list[
+        Any
+    ]  # this should be prefect.Task, but that's not working with pydantic v2 for now
 
     def __init__(self, config: FlowConfig) -> None:
+        from prefect import Task
+
         stages = config.stages
         self.tasks = []
         for stage in stages:
             self.tasks.append(
-                task(
+                Task(
                     stage.resolve(),
                     name=stage.name,
                 )
@@ -28,7 +32,9 @@ class Workflow:
 
     def run(self) -> None:
         """Function to execute the workflow. Wraps __call__ to convert the workflow into a Prefect flow."""
-        f = flow(
+        from prefect import Flow
+
+        f = Flow(
             self.__call__,
             name="config name",
             flow_run_name="fasthep-flow",
