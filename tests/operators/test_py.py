171fd84f3ac9043284e5f192f1a7b8b4cd4b5e57
Orion Yeung <11580988+orionyeung001@users.noreply.github.com>
2024-02-25 13:14:59 -0600

    feat: skeleton for PythonOperator, related to issue #12
    wrote few tests for testing builtin and importable with failure
    - `__call__` is unimplemented

---
+++ tests/operators/test_py.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from fasthep_flow.operators.python import PythonOperator
+
+
+def test_python_operator():
+    operator = PythonOperator(callable="print", arguments=["Hello World!"])
+    result = operator()
+    assert result["stdout"] == "Hello World!\n"
+    assert result["stderr"] == ""
+
+def test_python_operator_aliased():
+    operator = PythonOperator(callable="lambda x: print(np.count_nonzero(x))", arguments=["[0,1,1]"], aliases={'numpy': 'np'})
+    result = operator()
+    assert result["stdout"] == "2\n"
+    assert result["stderr"] == ""
+
+def test_python_operator_import_error():
+    operator = PythonOperator(callable="lambda x: print(np.count_nonzero(x))", arguments=["[0,1,1]"])
+    result = operator()
+    assert result["stdout"] == ""
+    assert result["stderr"] == "ImportError"
+
+def test_python_operator_attrib_error():
+    operator = PythonOperator(callable="lambda x: print(np.nonimplemented(x))", arguments=["[0,1,1]"])
+    result = operator()
+    assert result["stdout"] == ""
+    assert result["stderr"] == "AttributeError"
+
+def test_python_operator_callable_failure():
+    operator = PythonOperator(callable="print(\"Hello World!\n\")")
+    result = operator()
+    assert result["stdout"] == ""
+    assert result["stderr"] == "AttributeError, not callable"
