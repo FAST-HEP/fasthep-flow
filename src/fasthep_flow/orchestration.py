bb852466b741b4a08132588b1852bdece1297206
kreczko <lkreczko@googlemail.com>
2024-10-15 13:54:13 +0100

    refactor: replace prefect with hamilton

--- src/fasthep_flow/orchestration.py
+++ src/fasthep_flow/orchestration.py
@@ -5,42 +5,44 @@ e.g. Prefect, NetworkX, etc., to either execute them or visualize them.
 
 from __future__ import annotations
 
+import logging
 from typing import Any
 
-from .workflow import Workflow
+from hamilton import driver, telemetry
+
+from .workflow import Workflow, load_tasks_module
+
+telemetry.disable_telemetry()  # we don't want to send telemetry data for this example
+
+logger = logging.getLogger(__name__)
+
+# def get_runner(runner: str) -> Any:
+#     """Get the task runner for the given name."""
+#     from prefect.task_runners import ConcurrentTaskRunner, SequentialTaskRunner
+#     from prefect_dask import DaskTaskRunner
+
+#     runners: dict[str, Any] = {
+#         "Dask": DaskTaskRunner,
+#         "Sequential": SequentialTaskRunner,
+#         "Concurrent": ConcurrentTaskRunner,
+#     }
+
+#     return runners[runner]
 
 
-def get_runner(runner: str) -> Any:
-    """Get the task runner for the given name."""
-    from prefect.task_runners import ConcurrentTaskRunner, SequentialTaskRunner
-    from prefect_dask import DaskTaskRunner
+# def create_dask_cluster() -> Any:
+#     cluster = LocalCluster()
+#     client = Client(cluster)
+#     logger.info(client.cluster)
+#     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
-    runners: dict[str, Any] = {
-        "Dask": DaskTaskRunner,
-        "Sequential": SequentialTaskRunner,
-        "Concurrent": ConcurrentTaskRunner,
-    }
-
-    return runners[runner]
+#     return client
 
 
-def prefect_workflow(workflow: Workflow) -> Any:
-    """Convert a workflow into a Prefect flow."""
-    from prefect import Flow, Task
-
-    def prefect_wrapper() -> Any:
-        """Function to execute all tasks in the workflow."""
-        for task in workflow.tasks:
-            # TODO: add subflows
-            prefect_task = Task(task.payload, name=task.name)
-            prefect_task()
-
-    return Flow(
-        prefect_wrapper,
-        name="config name",
-        flow_run_name="fasthep-flow",
-        version="0.0.1",
-    )
+def workflow_to_hamilton_dag(workflow: Workflow) -> Any:
+    """Convert a workflow into a Hamilton flow."""
+    task_functions = load_tasks_module(workflow)
+    return driver.Builder().with_modules(task_functions).with_cache().build()
 
 
 # def gitlab_ci_workflow(workflow: Workflow):
