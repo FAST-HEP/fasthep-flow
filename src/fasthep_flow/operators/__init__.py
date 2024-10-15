97036cbb52e4f4728f2e2160bd55f0af60f52bb3
kreczko <lkreczko@googlemail.com>
2024-10-15 16:49:39 +0100

    feat: add simple PythonOperator

--- src/fasthep_flow/operators/__init__.py
+++ src/fasthep_flow/operators/__init__.py
@@ -1,8 +1,10 @@
 """Module for defining basic operators."""
+
 from __future__ import annotations
 
 from .base import Operator
 from .bash import BashOperator, LocalBashOperator
+from .py_call import PythonOperator
 
 # only Operator is exposed to the user, everything else is imported directly by the workflow
-__all__ = ["BashOperator", "LocalBashOperator", "Operator"]
+__all__ = ["BashOperator", "LocalBashOperator", "Operator", "PythonOperator"]
---
+++ src/fasthep_flow/operators/py_call.py
@@ -0,0 +1,36 @@
+"""Python related operators."""
+from __future__ import annotations
+
+import io
+from collections.abc import Callable
+from contextlib import redirect_stderr, redirect_stdout
+from typing import Any
+
+from .base import Operator
+
+
+class PythonOperator(Operator):
+    python_callable: Callable[..., Any]
+    arguments: list[Any]
+
+    def __init__(self, **kwargs: Any):
+        self.configure(**kwargs)
+
+    def configure(self, **kwargs: Any) -> None:
+        """Configure the operator."""
+        self.python_callable = kwargs.pop("python_callable")
+        self.arguments = kwargs.pop("arguments")
+
+    def __call__(self, **kwargs: Any) -> dict[str, Any]:
+        stdout, stderr = io.StringIO(), io.StringIO()
+        with redirect_stdout(stdout), redirect_stderr(stderr):
+            result = self.python_callable(*self.arguments)
+        result = self.python_callable(*self.arguments)
+        return {
+            "result": result,
+            "stdout": stdout.getvalue(),
+            "stderr": stderr.getvalue(),
+        }
+
+    def __repr__(self) -> str:
+        return f"PythonOperator(python_callable={self.python_callable}, arguments={self.arguments})"
