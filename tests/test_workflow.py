1cda32d2a0d1bb68d446ff14d030e2403a3ba7da
kreczko <lkreczko@googlemail.com>
2025-02-10 16:36:39 +0000

    tests: add workflow tests for parallel tasks

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -3,16 +3,20 @@ from __future__ import annotations
 import pytest
 
 from fasthep_flow import FlowConfig
-from fasthep_flow.workflow import create_workflow
+from fasthep_flow.workflow import create_workflow, get_task_source
 
 
 @pytest.fixture()
-def config(simple_dict_config):
-    return FlowConfig.from_dictconfig(simple_dict_config)
+def workflow(simple_dict_config):
+    return create_workflow(FlowConfig.from_dictconfig(simple_dict_config))
 
 
-def test_create_workflow(config):
-    workflow = create_workflow(config)
+@pytest.fixture()
+def parallel_workflow(parallel_dict_config):
+    return create_workflow(FlowConfig.from_dictconfig(parallel_dict_config))
+
+
+def test_create_workflow(workflow):
     assert workflow.tasks
     assert len(workflow.tasks) == 1
     task = workflow.tasks[0]
@@ -23,8 +27,7 @@ def test_create_workflow(config):
     assert params["arguments"] == ["Hello World!"]
 
 
-def test_run_workflow(config):
-    workflow = create_workflow(config)
+def test_run_workflow(workflow):
     results = workflow.run()
     assert results
     assert len(results) == 1
@@ -32,7 +35,22 @@ def test_run_workflow(config):
     assert result["stdout"] == "Hello World!\n"
 
 
-def test_task_names(config):
-    workflow = create_workflow(config)
+def test_task_names(workflow):
     assert workflow.task_names
     assert workflow.task_names == ["printEcho"]
+
+
+def test_get_task_source(workflow):
+    source = get_task_source(workflow.tasks[0], "printEcho")
+    assert "def printEcho" in source
+
+
+def test_parallel(parallel_workflow):
+    assert parallel_workflow.tasks
+    assert len(parallel_workflow.tasks) == 5
+    assert parallel_workflow.tasks[0].name == "A"
+    assert parallel_workflow.tasks[2].name == "C"
+    assert parallel_workflow.tasks[4].name == "Y"
+    assert parallel_workflow.tasks[0].needs == []
+    assert parallel_workflow.tasks[2].needs == ["A"]
+    assert parallel_workflow.tasks[4].needs == ["C", "D"]
