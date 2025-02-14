ff8d36f015004d919cca57b354f27d75940ac339
kreczko <lkreczko@googlemail.com>
2025-02-14 13:37:40 +0000

    refactor(LoggingPlugin): print -> logger

--- src/fasthep_flow/plugins/logging.py
+++ src/fasthep_flow/plugins/logging.py
@@ -3,14 +3,16 @@ from __future__ import annotations
 from collections.abc import Callable
 from typing import Any
 
+from loguru import logger
+
 from ._base import PluginInterface
 
 
 class LoggingPlugin(PluginInterface):
     def before(self, func: Callable[..., Any], *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
-        print(  # noqa: T201
-            f"Running {func.__name__} with args {args} and kwargs {kwargs}"
-        )
+        logger.info(f"Running {func.__name__} with args {args} and kwargs {kwargs}")
 
     def after(self, func: Callable[..., Any], result: Any, *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
-        print(f"Finished {func.__name__} with result {result}")  # noqa: T201
+        if result:
+            logger.success(f"Finished {func.__name__} with result {result}")
+        logger.warning(f"Finished {func.__name__} with no result")
