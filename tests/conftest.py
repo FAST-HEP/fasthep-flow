063618631ed08e85bb94a423d0ee6393d2ca95fc
kreczko <lkreczko@googlemail.com>
2025-02-17 16:38:44 +0000

    feat(config): add plugins_by_task and plugin tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -39,3 +39,19 @@ def parallel_config(parallel_config_yaml) -> str:
 @pytest.fixture()
 def parallel_dict_config(parallel_config) -> Any:
     return yaml.safe_load(parallel_config)
+
+
+@pytest.fixture()
+def plugin_config_yaml() -> Path:
+    return current_dir / "data" / "plugins.yaml"
+
+
+@pytest.fixture()
+def plugin_config(plugin_config_yaml) -> str:
+    with Path.open(plugin_config_yaml, "r") as file:
+        return file.read()
+
+
+@pytest.fixture()
+def plugin_dict_config(plugin_config) -> Any:
+    return yaml.safe_load(plugin_config)
